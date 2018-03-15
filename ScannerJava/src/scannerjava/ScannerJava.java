0065bfd2a942679483f50c16519518b379fc0545
Renaud BERNARD <renaud.bernard@p59production.com>
2018-03-15 11:56:59 +0100

    Create project
    Create ScannerJava project with NetBeans.

---
+++ ScannerJava/src/scannerjava/ScannerJava.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scannerjava;
+
+/**
+ *
+ * @author renob
+ */
+public class ScannerJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
